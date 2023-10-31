06baf5327af2dbbcf6581daae5bcdf5d90c0c372
oscarmacas <128268631+oscarmacas@users.noreply.github.com>
2023-10-30 22:39:30 -0500

    ordenado

--- aliases.bat
+++ aliases.bat
@@ -1,40 +1,47 @@
 @echo off
 
-:: GLOBAL ALIAS
-doskey ls=dir /B
-doskey alias=micro "C:\aliases\aliases.bat"
-doskey fe=fzf -e $B clip
-doskey dw=cd %userprofile%\Downloads
-doskey wifipass=wifipassword.bat $*
-doskey admin=powershell start cmd -v runAs
-doskey stup=cd "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+:: NAVEGACION DE DIRECTORIOS
 doskey usr=cd %userprofile%
-doskey wifinames=netsh wlan show profiles
+doskey docs=cd %userprofile%\Documents
+doskey dw=cd %userprofile%\Downloads
+doskey img=cd %userprofile%\Pictures
+doskey mus=cd %userprofile%\Music
+doskey vid=cd %userprofile%\Videos
+doskey desk=cd %userprofile%\Desktop
+doskey inv=cd "%userprofile%\Documents\inventarios\"
+doskey imp=cd C:\php\web\amfphp\services\SIGNUM\IMPRESIONES\
+doskey c=cd C:\
+doskey stup=cd "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 doskey apdt=cd %appdata%
 doskey .=start .
 doskey q=exit
-doskey c=cd C:\
+doskey ls=dir /B
+doskey ll=dir /B /od
+doskey rd=rd $* /S /Q
+
+:: ALIAS
+doskey alias=micro "C:\aliases\aliases.bat"
+doskey al=bat "C:\aliases\aliases.bat"
+doskey udalias=curl https://raw.githubusercontent.com/oscarmacas/scripts/main/aliases.bat>C:\aliases\aliases.bat $T cmd $T echo El archivo alias ha sido actualizado.
+
+:: GLOBAL ALIAS
+doskey fe=fzf -e $B clip
+doskey wifipass=wifipassword.bat $*
+doskey admin=powershell start cmd -v runAs
+doskey wifinames=netsh wlan show profiles
 doskey st=speedtest
 doskey sp=winget search $*
-doskey al=more "C:\aliases\aliases.bat"
 doskey i=micro $*
 doskey edit=For /F %%n IN ('fzf') DO @(micro %%n)
 doskey wf=netsh wlan show profile key=clear $* $B findstr /i /L /c:"Nombre de SSID" /c:"clave"
 doskey enviar=croc send --code oscarm $*
 doskey recibir=croc --yes oscarm
-doskey inv=cd "%userprofile%\Documents\inventarios\"
 doskey k=taskkill /IM cmd.exe
 doskey io=copy %userprofile%\Documents\inventarios\plantilla_inventarios.xlsx %userprofile%\Documents\inventarios\"inventario_$*_%date:~0,2%_%date:~3,2%_%date:~6,4%.xlsx" $T %userprofile%\Documents\inventarios\"inventario_$*_%date:~0,2%_%date:~3,2%_%date:~6,4%.xlsx"
-doskey yd=yt-dlp -f m4a "$*"
+doskey yd=yt-dlp -f m4a -o "%(title)s.%(ext)s" "$*"
 doskey cc=7z a -tzip $1.zip $2 $3 $4 $5 $6
 doskey xx=7z x $*.zip -o$*
-doskey r=curl -L rentry.co/$*/raw
+doskey r=curl -L rentry.co/$*/raw $B bat -l markdown
 doskey mr=curl -L rentry.co/$*/raw $B micro
-doskey desk=cd %userprofile%\Desktop
 doskey impresiones=curl https://raw.githubusercontent.com/oscarmacas/files/main/IMPRESIONES.php>C:\php\web\amfphp\services\SIGNUM\IMPRESIONES\IMPRESIONES.php
-doskey imp=cd C:\php\web\amfphp\services\SIGNUM\IMPRESIONES\
-doskey docs=cd %userprofile%\Documents
-doskey img=cd %userprofile%\Pictures
-doskey mus=cd %userprofile%\Music
-doskey vid=cd %userprofile%\Videos
-doskey udalias=curl https://raw.githubusercontent.com/oscarmacas/scripts/main/aliases.bat>C:\aliases\aliases.bat $T cmd $T echo El archivo alias ha sido actualizado.
+doskey ipp=curl icanhazip.com
