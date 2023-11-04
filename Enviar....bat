24f53ca34eef86ef7b98891aa527b78546a133db
oscarmacas <128268631+oscarmacas@users.noreply.github.com>
2023-11-04 15:49:41 -0500

    Update Enviar....bat

--- Enviar....bat
+++ Enviar....bat
@@ -1,14 +1,6 @@
 @echo off
 setlocal
 
-rem Revisar si croc está instalado
-where croc >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Croc no está instalado en el sistema.
-    pause
-    exit /b
-)
-
 rem Enviar archivo usando croc
 croc send --code oscarm %1
 
