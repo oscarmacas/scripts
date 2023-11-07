73366408f90cc5099b1b87107c8b13a167a21019
oscarmacas <128268631+oscarmacas@users.noreply.github.com>
2023-11-07 11:27:37 -0500

    aria

--- aliases.bat
+++ aliases.bat
@@ -54,3 +54,10 @@ doskey ccs=choco search $*
 doskey cin=powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd /d %CD% && choco install $* -y'"
 doskey win=powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd /d %CD% && winget install $*'"
 doskey wws=winget search $*
+doskey k=taskkill /IM WindowsTerminal.exe
+doskey ..= cd ..
+doskey ...= cd ..\..
+doskey 7zl=7z l $*
+doskey 7za=7z a -tzip $1 $2 $3 $4 $5 $6
+doskey dl=aria2c --max-concurrent-downloads=16 --split=16 --max-connection-per-server=16 --min-split-size=1M --max-download-limit=0 $*
+doskey dll=aria2c --max-concurrent-downloads=16 --split=16 --max-connection-per-server=16 --min-split-size=1M --max-download-limit=0 -i $*
