7997c0ffc8741de77c24ce3a203e1224d34e7bf7
oscarmacas <128268631+oscarmacas@users.noreply.github.com>
2024-09-11 16:54:38 -0500

    UD

--- aliases.bat
+++ aliases.bat
@@ -1,9 +1,5 @@
 @echo off
 
-:: $T means &
-:: $B means |
-:: $G means >
-
 :: NAVEGACION DE DIRECTORIOS
 doskey usr=cd %userprofile%
 doskey docs=cd %userprofile%\Documents
@@ -25,10 +21,10 @@ doskey rd=rd $* /S /Q
 doskey ls=ls --color=auto -H -F
 
 :: ALIAS
-doskey alias=cmd /c where nvim $Gnul 2$Gnul $T$T (nvim C:\aliases\aliases.bat) $B$B (more C:\aliases\aliases.bat)
+doskey alias=micro "C:\aliases\aliases.bat"
 doskey al=bat "C:\aliases\aliases.bat"
-doskey udalias=curl https://raw.githubusercontent.com/oscarmacas/scripts/main/aliases.bat$GC:\aliases\aliases.bat $T echo El archivo alias ha sido actualizado. $T cmd
-doskey alinfo=dir "C:\aliases\aliases.bat"
+doskey udalias=curl https://raw.githubusercontent.com/oscarmacas/scripts/main/aliases.bat$GC:\aliases\aliases.bat $T echo El archivo alias ha sido actualizado. Ejecute cmd.
+doskey lll=echo ================^==^==       ACTUAL         ^==^==^=============
 
 :: GLOBAL ALIAS
 doskey fe=fzf -e $B clip
@@ -41,15 +37,13 @@ doskey i=micro $*
 doskey edit=For /F %%n IN ('fzf') DO @(micro %%n)
 doskey wf=netsh wlan show profile key=clear $* $B findstr /i /L /c:"Nombre de SSID" /c:"clave"
 doskey enviar=croc send --code oscarm $*
-doskey recibir=if not exist "%userprofile%\Desktop\recibidos" md "%userprofile%\Desktop\recibidos" $T cd /d "%userprofile%\Desktop\recibidos" $T croc --yes oscarm $T if errorlevel 1 echo Croc command failed $T start .
+doskey recibir=croc --yes oscarm
 doskey kk=taskkill /IM cmd.exe
 doskey io=copy %userprofile%\Documents\inventarios\plantilla_inventarios.xlsx %userprofile%\Documents\inventarios\"inventario_$*_%date:~-10,2%_%date:~-7,2%_%date:~-4,4%.xlsx" $T %userprofile%\Documents\inventarios\"inventario_$*_%date:~-10,2%_%date:~-7,2%_%date:~-4,4%.xlsx"
 doskey yd=yt-dlp -f m4a -o "%(title)s.%(ext)s" "$*"
 doskey cc=7z a -tzip $1.zip $2 $3 $4 $5 $6
-doskey xx=if not exist "$1" (echo File not found: $1) else if "$2"=="" (for %%I in ("$1") do 7z x "%%I" -o"%%~dpnI") else if exist "$2\" (7z x "$1" -o"$2") else (echo Invalid output directory: $2) $t cd $1 $t dir /b
+doskey xx=7z x $*.zip -o$*
 doskey r=curl -L rentry.co/$*/raw $B bat -l markdown
-doskey oslink=curl -L rentry.co/oslink/raw
-doskey osinstall=curl -L rentry.co/osinstall/raw
 doskey mr=curl -L rentry.co/$*/raw $B micro
 doskey impresiones=curl https://raw.githubusercontent.com/oscarmacas/files/main/IMPRESIONES.php$GC:\php\web\amfphp\services\SIGNUM\IMPRESIONES\IMPRESIONES.php
 doskey ipp=curl icanhazip.com
@@ -60,8 +54,6 @@ doskey wws=winget search $*
 doskey k=taskkill /IM WindowsTerminal.exe
 doskey ccs=choco search $*
 doskey cin=powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd /d %CD% && choco install $* -y'"
-doskey cup=powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd /d %CD% && choco upgrade $* -y'"
-doskey cupkg=powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd /d %CD% && choco upgrade croc -y && choco upgrade 7zip -y && choco upgrade aria2 -y && yt-dlp -U && winget upgrade RustDesk.RustDesk'"
 doskey win=powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd /d %CD% && winget install $*'"
 doskey wws=winget search $*
 doskey k=taskkill /IM WindowsTerminal.exe
@@ -72,17 +64,5 @@ doskey 7za=7z a -tzip $1 $2 $3 $4 $5 $6
 doskey dl=aria2c --max-concurrent-downloads=16 --split=16 --max-connection-per-server=16 --min-split-size=1M --max-download-limit=0 $*
 doskey dll=aria2c --max-concurrent-downloads=16 --split=16 --max-connection-per-server=16 --min-split-size=1M --max-download-limit=0 -i $*
 doskey pth=setx /M path "%path%;$*"
-doskey office10=aria2c --max-concurrent-downloads=16 --split=16 --max-connection-per-server=16 --min-split-size=1M --max-download-limit=0 -j 1 -i magnet:?xt=urn:btih:27113327FBC680659464B72F63A2C1C644AC8EF8^&dn=Microsoft%20OFFICE%202010%20%20Pro%20Plus%20PRECRACKED^&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337^&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce^&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce^&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce^&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce^&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce^&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce^&tr=udp%3A%2F%2Fexodus.desync.com%3A6969^&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce
-doskey nprint=cd "C:\php\web\amfphp\services\SIGNUM\IMPRESIONES\" $T ren IMPRESIONES.php IMPRESIONES.php.dot
-doskey yprint=cd "C:\php\web\amfphp\services\SIGNUM\IMPRESIONES\" $T ren IMPRESIONES.php.dot IMPRESIONES.php
-doskey stxt=croc send --code oscarm --text $*
-doskey rtxt=croc --yes oscarm
-doskey conv="C:\script\todoconvert.bat"
-doskey udahk=taskkill /im "autohotkey.exe" $T taskkill /im "autohotkey64.exe" $T winget upgrade autohotkey.autohotkey $T del "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\script.ahk" $T curl https://raw.githubusercontent.com/oscarmacas/scripts/main/script.ahk$G"%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\script.ahk" $T "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\script.ahk"
-doskey udss=taskkill /im "autohotkey.exe" $T taskkill /im "autohotkey64.exe" $T del "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\script.ahk" $T curl https://raw.githubusercontent.com/oscarmacas/scripts/main/script.ahk$G"%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\script.ahk" $T "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\script.ahk"
-doskey inputy=curl https://raw.githubusercontent.com/oscarmacas/scripts/main/inv_input.ahk$G"C:\script\inv_input.ahk" $T "C:\script\inv_input.ahk"
-doskey inputn=taskkill /im "autohotkey.exe" $T taskkill /im "autohotkey64.exe" $T "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\script.ahk
-doskey xtbat=mkdir "C:\script\lib\" $t cd "C:\script\lib\" $t curl https://raw.githubusercontent.com/oscarmacas/scripts/main/lib/extract_zip.bat --output extract_zip.bat
-doskey xt="C:\script\lib\extract_zip.bat" $*
-doskey udnvim=cmd /c "if not exist "%USERPROFILE%\AppData\Local\nvim" mkdir "%USERPROFILE%\AppData\Local\nvim" $t curl -o "%USERPROFILE%\AppData\Local\nvim\init.vim" https://raw.githubusercontent.com/oscarmacas/scripts/main/lib/init.vim"
-doskey nv=nvim $*
+doskey pwd=cd
+doskey cwd=cd $B clip
