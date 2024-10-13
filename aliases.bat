a5472321c02a534297942ef28e46c0994754ba0a
oscarmacas <128268631+oscarmacas@users.noreply.github.com>
2024-10-13 13:47:16 -0500

    Update aliases.bat

--- aliases.bat
+++ aliases.bat
@@ -97,4 +97,4 @@ doskey fnm=rg -i "$*" %AppData%\ovscript\nm
 doskey gnm=cmd /c "if not exist %AppData%\ovscript\nm mkdir %AppData%\ovscript\nm $T cd %AppData%\ovscript\nm $T croc --yes oscarm"
 doskey irg=powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd /d %CD% && winget install BurntSushi.ripgrep.MSVC'"
 doskey cod=bat C:\script\lib\codes\codes $B fzf -e
-doskey codin=cmd /c "if not exist C:\script\lib\ mkdir C:\script\lib\ $T wget https://github.com/oscarmacas/files/raw/main/codes.zip $T cd C:\script\lib\ $T 7z x codes.zip -ocodes"
+doskey codin=cmd /c "if not exist C:\script\lib\ mkdir C:\script\lib\ $T cd C:\script\lib\ $T wget https://github.com/oscarmacas/files/raw/main/codes.zip $T 7z x codes.zip -ocodes"
