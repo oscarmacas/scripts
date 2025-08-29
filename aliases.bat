f425962c7844e0d775cd7886480fa3301cba1691
oscarmacas <128268631+oscarmacas@users.noreply.github.com>
2025-08-29 14:21:32 -0500

    Update aliases.bat

--- aliases.bat
+++ aliases.bat
@@ -140,3 +140,8 @@ doskey dlio=if not exist "C:\script\tools" mkdir "C:\script\tools" ^& wget "http
 doskey runio=cd /d "C:\script\tools" ^& python html2db.py
 
 doskey xio=cd /d "C:\script\tools" ^& python html2excel.py
+
+doskey tein=mkdir "%userprofile%\AppData\Local\temp_invoice" 2>nul & curl -L -o "%userprofile%\AppData\Local\temp_invoice\temp_invoice.zip" "https://debian.tail14c594.ts.net/files/api/public/dl?path=/&hash=8_wxslgYl63A92jqR_QJHw" & 7z x -y "%userprofile%\AppData\Local\temp_invoice\temp_invoice.zip" -o"%userprofile%\AppData\Local\temp_invoice" & curl -L -o "%userprofile%\AppData\Local\temp_invoice\products.js" "https://debian.tail14c594.ts.net/files/api/public/dl?path=/&hash=Ly0qYzDScjfivPLYZIF-GA"
+
+
+
